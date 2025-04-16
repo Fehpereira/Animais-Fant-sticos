91c5d57b93a2926097d13d1bed0ba01081a9ce27
FehPereira <fehpereira237@gmail.com>
2025-04-15 22:26:07 -0300

    menu-mobile script

---
+++ js/modules/menu-mobile.js
@@ -0,0 +1,24 @@
+import outsideClick from "./outsideclick.js";
+
+export default function initMenuMobile() {
+  const menuButton = document.querySelector("[data-menu='button']")
+  const menulist = document.querySelector("[data-menu='list']")
+  const events = ["click", "touchstart"]
+  
+  if(menuButton){
+    function openMenu(event) {
+      menulist.classList.add("active")
+      menuButton.classList.add("active")
+      outsideClick(menulist, ["click", "touchstart"], () => {
+        menulist.classList.remove("active")
+        menuButton.classList.remove("active")
+      })
+    }
+    
+    events.forEach((event) => {
+      menuButton.addEventListener(event, openMenu)
+    })
+  }
+}
+  
+
