c65bc1bacb62e1b17e90bbe471076d750291eef5
FehPereira <fehpereira237@gmail.com>
2025-04-30 19:38:36 -0300

    .

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -10,7 +10,7 @@ export default function initAccordion() {
     const activeAccordion = (event) => {
       event.currentTarget.classList.toggle(activeClass);
       event.currentTarget.nextElementSibling.classList.toggle(activeClass);
-    };
+    }
 
     accordionList.forEach((item) => {
       item.addEventListener('click', activeAccordion);
